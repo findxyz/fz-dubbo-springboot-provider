32ea9027c135936a6f34df08ff7e162dedfed69e
fz <natureorz@foxmail.com>
2017-10-23 11:46:29 +0800

    Initial commit

---
+++ src/main/java/xyz/fz/dubbo/service/DemoService.java
@@ -0,0 +1,7 @@
+package xyz.fz.dubbo.service;
+
+public interface DemoService {
+    String sayHello(String name);
+
+    String sayTxHello(String name);
+}
---
+++ src/main/java/xyz/fz/dubbo/service/impl/DemoServiceImpl.java
@@ -0,0 +1,36 @@
+package xyz.fz.dubbo.service.impl;
+
+import com.alibaba.dubbo.config.annotation.Service;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.transaction.annotation.Transactional;
+import xyz.fz.dubbo.service.DemoService;
+
+import javax.annotation.Resource;
+import java.util.HashMap;
+import java.util.Map;
+
+@Service(interfaceName = "xyz.fz.dubbo.service.DemoService", version = "1.0.0")
+public class DemoServiceImpl implements DemoService {
+
+    @Resource
+    private NamedParameterJdbcTemplate namedParameterJdbcTemplate;
+
+    @Override
+    public String sayHello(String name) {
+        return "Hello " + name;
+    }
+
+    @Override
+    @Transactional
+    public String sayTxHello(String name) {
+        String sql = "insert into t_test values(:name) ";
+        Map<String, Object> params = new HashMap<>();
+        params.put("name", name);
+        namedParameterJdbcTemplate.update(sql, params);
+        int a = 1/0;
+        Map<String, Object> params2 = new HashMap<>();
+        params2.put("name", name + "a");
+        namedParameterJdbcTemplate.update(sql, params2);
+        return name;
+    }
+}
